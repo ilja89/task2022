e7e5838807c3d5ef7dd47ac2941c82dbfc6bfb9a
Joosep Alviste <joosep.alviste@gmail.com>
2017-03-07 01:11:01 +0200

    Upgrade code to 5.4

--- plugin/config/database.php
+++ plugin/config/database.php
@@ -6,19 +6,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | PDO Fetch Style
-    |--------------------------------------------------------------------------
-    |
-    | By default, database results will be returned as instances of the PHP
-    | stdClass object; however, you may desire to retrieve records in an
-    | array format for simplicity. Here you can tweak the fetch style.
-    |
-    */
-
-    'fetch' => PDO::FETCH_OBJ,
-
-    /*
-    |--------------------------------------------------------------------------
     | Default Database Connection Name
     |--------------------------------------------------------------------------
     |
@@ -61,8 +48,8 @@ return [
             'database' => env('DB_DATABASE', $CFG->dbname),
             'username' => env('DB_USERNAME', $CFG->dbuser),
             'password' => env('DB_PASSWORD', $CFG->dbpass),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => env('DB_TABLE_PREFIX', $CFG->prefix),
             'strict' => true,
             'engine' => null,
@@ -70,7 +57,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -109,10 +96,10 @@ return [
 
     'redis' => [
 
-        'cluster' => false,
+        'client' => 'predis',
 
         'default' => [
-            'host' => env('REDIS_HOST', 'localhost'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
