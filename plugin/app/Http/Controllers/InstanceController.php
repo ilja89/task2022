4a23890d7c7870d8f781986e66f5bd1f790b4fc3
Joosep Alviste <joosep.alviste@gmail.com>
2017-03-08 19:06:48 +0200

    Fix small mistake

--- plugin/app/Http/Controllers/InstanceController.php
+++ plugin/app/Http/Controllers/InstanceController.php
@@ -191,11 +191,17 @@ class InstanceController extends Controller
      */
     private function getCharonFromRequest()
     {
+        if ($this->request->input('extra') === null) {
+            $extra = '';
+        } else {
+            $extra = $this->request->input('extra');
+        }
+        
         return new Charon([
             'name'                => $this->request['name'],
             'description'         => $this->request['description']['text'],
             'project_folder'      => $this->request['project_folder'],
-            'extra'               => $this->request->input('extra', ''),
+            'extra'               => $extra,
             'tester_type_code'    => $this->request['tester_type'],
             'grading_method_code' => $this->request['grading_method'],
             'timemodified'        => Carbon::now()->timestamp,
