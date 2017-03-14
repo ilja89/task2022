fade43a9cfff4903faf63732f7c31b621f74422b
Joosep Alviste <joosep.alviste@gmail.com>
2017-03-14 18:26:50 +0200

    Refactor directives into module

--- plugin/resources/assets/js/popup.js
+++ plugin/resources/assets/js/popup.js
@@ -6,10 +6,10 @@ import router from './popup/routes';
 import Popup from './popup/Popup.vue';
 
 import PopupContext from './classes/popupContext';
-import highlightDirective from './directives';
+import { HighlightDirective } from './directives';
 
 Vue.use(VueRouter);
-Vue.directive('highlightjs', highlightDirective);
+Vue.directive('highlightjs', HighlightDirective);
 
 window.VueEvent = new Vue();
 
@@ -21,39 +21,6 @@ const app = new Vue({
     data: {
         context: new PopupContext(window.course_id),
     },
-    //
-    // mounted() {
-    //     this.registerEventListeners();
-    // },
-    //
-    // methods: {
-    //     registerEventListeners() {
-    //         VueEvent.$on('student-was-changed', student => {
-    //             this.context.active_student = student;
-    //             this.context.active_submission = null;
-    //         });
-    //         VueEvent.$on('charon-was-changed', charon => {
-    //             this.context.active_charon = charon;
-    //             this.context.active_submission = null;
-    //         });
-    //         VueEvent.$on('submission-was-selected', submission => {
-    //             this.context.active_submission = submission;
-    //         });
-    //         VueEvent.$on('change-page', pageName => {
-    //             this.context.active_page = pageName;
-    //         });
-    //         VueEvent.$on('show-notification', message => {
-    //             this.notification_text = message;
-    //             this.notification_show = true;
-    //             setTimeout(() => {
-    //                 this.notification_show = false;
-    //             }, 2000);
-    //         });
-    //         VueEvent.$on('close-notification', () => this.notification_show = false);
-    //         VueEvent.$on('show-loader', () => this.loaderVisible += 1);
-    //         VueEvent.$on('hide-loader', () => this.loaderVisible -= 1);
-    //     }
-    // },
 
     router
 });
