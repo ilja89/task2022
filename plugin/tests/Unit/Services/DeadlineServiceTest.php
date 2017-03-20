a456baa01be955bf2fbb489b5819537f02704f5a
Joosep Alviste <joosep.alviste@gmail.com>
2017-03-20 23:47:07 +0200

    Test deadline service

--- plugin/tests/Unit/Services/DeadlineServiceTest.php
+++ plugin/tests/Unit/Services/DeadlineServiceTest.php
@@ -10,6 +10,7 @@ use TTU\Charon\Services\DeadlineService;
 
 class DeadlineServiceTest extends TestCase
 {
+    /** @var DeadlineService */
     protected $deadlineService;
 
     public function setUp()
@@ -20,10 +21,30 @@ class DeadlineServiceTest extends TestCase
 
     public function testCreatesDeadlinesWithCorrectDeadlines()
     {
-        $charon = m::mock(Charon::class);
+        $this->createApplication();
+        \Config::set('app.timezone', 'Europe/Tallinn');
+
+        $charon = m::mock(Charon::class,
+            ['deadlines' => m::mock('deadlines')->shouldReceive('save')->once()->getMock()]
+        );
         $deadlineArray = [
             'deadline_time' => Carbon::now()->addHour()->format('d-m-Y H:i'),
             'percentage' => 50
         ];
+
+        $this->deadlineService->createDeadline($charon, $deadlineArray);
+    }
+
+    public function testDoesNotCreateDeadlinesWhenIncorrect()
+    {
+        $charon = m::mock(Charon::class,
+            ['deadlines' => m::mock('deadlines')->shouldReceive('save')->never()->getMock()]
+        );
+        $deadlineArray = [
+            'deadline_time' => 'some incorrect value',
+            'percentage' => 'Haha'
+        ];
+
+        $this->deadlineService->createDeadline($charon, $deadlineArray);
     }
 }
