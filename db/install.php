db3b162547ed9e70300d1d0789796245b77ca4ac
Joosep Alviste <joosep.alviste@gmail.com>
2017-05-22 11:29:17 +0300

    Try install script

--- db/install.php
+++ db/install.php
@@ -10,6 +10,8 @@ require __DIR__ . '/../plugin/bootstrap/autoload.php';
  */
 function xmldb_charon_install() {
 
+    charon_install_composer_dependencies();
+
     $app = require __DIR__ . '/../plugin/bootstrap/app.php';
     $kernel = $app->make('Illuminate\Contracts\Console\Kernel');
 
@@ -18,4 +20,29 @@ function xmldb_charon_install() {
     $kernel->call('cache:clear');
 
     return true;
+}
+
+if (!function_exists('charon_install_composer_dependencies')) {
+    function charon_install_composer_dependencies() {
+
+        global $CFG;
+        $charonPath = $CFG->dirroot . '/mod/charon';
+
+        require_once "phar://" . $charonPath . "composer.phar/src/bootstrap.php";
+        chdir($charonPath);
+        putenv("COMPOSER_HOME={$charonPath}");
+        putenv("COMPOSER={$charonPath}composer.json");
+        putenv("COMPOSER_VENDOR_DIR={$charonPath}vendor");
+        putenv("OSTYPE=OS400");
+        $app = new \Composer\Console\Application();
+        $factory = new \Composer\Factory();
+        $output = $factory->createOutput();
+        $input = new \Symfony\Component\Console\Input\ArrayInput(array(
+            'command' => 'install',
+            '--no-progress' => true,
+            '--no-dev' => true
+        ));
+        $input->setInteractive(false);
+        $cmdret = $app->doRun($input,$output);
+    }
 }
