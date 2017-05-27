37ffedb00ee36614123b1b24d7348ebde27b6b59
Joosep Alviste <joosep.alviste@gmail.com>
2017-05-27 20:31:15 +0300

    Revert "Install composer packages on zip install"
    This reverts commit bd93411b0d428e508ecdb179df64792602a9b25a.

--- db/install.php
+++ db/install.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__ . '/../plugin/bootstrap/autoload.php';
+
 /**
  * Post install script. This is run after creating tables from install.xml.
  * Used to populate the database with classification data.
@@ -8,13 +10,14 @@
  */
 function xmldb_charon_install() {
 
-    global $CFG;
-
     charon_install_composer_dependencies();
 
-    exec('php ' . $CFG->dirroot . '/mod/charon/artisan db:seed --class=ClassificationsSeeder --force');
-    exec('php ' . $CFG->dirroot . '/mod/charon/artisan db:seed --class=PresetsSeeder --force');
-    exec('php ' . $CFG->dirroot . '/mod/charon/artisan cache:clear');
+    $app = require __DIR__ . '/../plugin/bootstrap/app.php';
+    $kernel = $app->make('Illuminate\Contracts\Console\Kernel');
+
+    $kernel->call('db:seed', ['--class' => 'ClassificationsSeeder']);
+    $kernel->call('db:seed', ['--class' => 'PresetsSeeder']);
+    $kernel->call('cache:clear');
 
     return true;
 }
@@ -23,51 +26,23 @@ if (!function_exists('charon_install_composer_dependencies')) {
     function charon_install_composer_dependencies() {
 
         global $CFG;
-        $charonPath = $CFG->dirroot . '/mod/charon/';
-
-        echo '<pre>';
-
-        charon_install_composer($charonPath);
+        $charonPath = $CFG->dirroot . '/mod/charon';
 
         require_once "phar://" . $charonPath . "composer.phar/src/bootstrap.php";
-
         chdir($charonPath);
         putenv("COMPOSER_HOME={$charonPath}");
         putenv("COMPOSER={$charonPath}composer.json");
         putenv("COMPOSER_VENDOR_DIR={$charonPath}vendor");
         putenv("OSTYPE=OS400");
-
         $app = new \Composer\Console\Application();
         $factory = new \Composer\Factory();
         $output = $factory->createOutput();
         $input = new \Symfony\Component\Console\Input\ArrayInput(array(
             'command' => 'install',
             '--no-progress' => true,
-//            '--no-dev' => true
+            '--no-dev' => true
         ));
-
         $input->setInteractive(false);
         $cmdret = $app->doRun($input,$output);
-
-        echo 'Packages installed.' . "\n";
-
-        echo '</pre>';
-    }
-}
-
-if (!function_exists('charon_install_composer')) {
-    function charon_install_composer($charonPath) {
-
-        if (file_put_contents($charonPath . "composer-installer.php", fopen("https://getcomposer.org/installer", 'r'))) {
-            echo "Downloaded composer installer...\n";
-        } else {
-            echo "Failed to download composer installer.\n";
-            return false;
-        }
-
-        $composerInstall = "COMPOSER_HOME=\"". $charonPath . "\" php " . $charonPath . "composer-installer.php --install-dir=" . $charonPath;
-
-        echo exec($composerInstall) . "\n";
-
     }
 }
