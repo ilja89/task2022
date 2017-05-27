82a822d99ce09f17ec5da50cc03c2fb3de71baee
Joosep Alviste <joosep.alviste@gmail.com>
2017-05-27 20:31:30 +0300

    Revert "Try install script"
    This reverts commit db3b162547ed9e70300d1d0789796245b77ca4ac.

--- db/install.php
+++ db/install.php
@@ -10,8 +10,6 @@ require __DIR__ . '/../plugin/bootstrap/autoload.php';
  */
 function xmldb_charon_install() {
 
-    charon_install_composer_dependencies();
-
     $app = require __DIR__ . '/../plugin/bootstrap/app.php';
     $kernel = $app->make('Illuminate\Contracts\Console\Kernel');
 
@@ -20,29 +18,4 @@ function xmldb_charon_install() {
     $kernel->call('cache:clear');
 
     return true;
-}
-
-if (!function_exists('charon_install_composer_dependencies')) {
-    function charon_install_composer_dependencies() {
-
-        global $CFG;
-        $charonPath = $CFG->dirroot . '/mod/charon';
-
-        require_once "phar://" . $charonPath . "composer.phar/src/bootstrap.php";
-        chdir($charonPath);
-        putenv("COMPOSER_HOME={$charonPath}");
-        putenv("COMPOSER={$charonPath}composer.json");
-        putenv("COMPOSER_VENDOR_DIR={$charonPath}vendor");
-        putenv("OSTYPE=OS400");
-        $app = new \Composer\Console\Application();
-        $factory = new \Composer\Factory();
-        $output = $factory->createOutput();
-        $input = new \Symfony\Component\Console\Input\ArrayInput(array(
-            'command' => 'install',
-            '--no-progress' => true,
-            '--no-dev' => true
-        ));
-        $input->setInteractive(false);
-        $cmdret = $app->doRun($input,$output);
-    }
 }
