18327ec0daadaca60f261a98d210ea716f4777f4
Joosep Alviste <joosep.alviste@gmail.com>
2018-08-21 20:22:33 +0300

    Plagiarism popup page with Charon select

---
+++ plugin/resources/assets/js/pages/popup/pages/PlagiarismPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+        <page-title title="Plagiarism"></page-title>
+
+        <plagiarism-results-section></plagiarism-results-section>
+
+    </div>
+</template>
+
+<script>
+    import { PageTitle } from '../partials'
+    import { PlagiarismResultsSection } from '../sections'
+
+    export default {
+        name: 'plagiarism_page',
+        components: { PageTitle, PlagiarismResultsSection },
+    }
+</script>
---
+++ plugin/resources/assets/js/pages/popup/sections/PlagiarismResultsSection.vue
@@ -0,0 +1,22 @@
+<template>
+    <popup-section
+        title="Plagiarism results"
+        subtitle="Here are the results for the latest plagiarism check."
+    >
+
+        <template slot="header-right">
+            <charon-select/>
+        </template>
+
+    </popup-section>
+</template>
+
+<script>
+    import { PopupSection } from '../layouts'
+    import { CharonSelect } from '../partials'
+
+    export default {
+        name: 'plagiarism-results-section',
+        components: { PopupSection, CharonSelect },
+    }
+</script>
