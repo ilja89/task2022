f0265a9558d64db61e0520a35553914c900b0c16
Karl Erik Õunapuu <karlerik@kreative.ee>
2018-11-08 15:19:49 +0200

    Update verson number

--- version.php
+++ version.php
@@ -3,5 +3,5 @@
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
 $plugin->version   = 2018110800;    // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2018082100;    // Requires this Moodle version
+$plugin->requires  = 2018051700;    // Requires this Moodle version
 $plugin->component = 'mod_charon';  // Full name of the plugin (used for diagnostics)
