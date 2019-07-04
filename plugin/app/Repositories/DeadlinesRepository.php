6e50a243775d5de2033a90d46247e098a45dfcbc
root <root@kreative-clean.europe-north1-a.c.perfect-eon-216011.internal>
2019-07-04 03:05:54 +0000

    Remove Calendar events along with Deadlines when Charon updated

--- plugin/app/Repositories/DeadlinesRepository.php
+++ plugin/app/Repositories/DeadlinesRepository.php
@@ -3,6 +3,7 @@
 namespace TTU\Charon\Repositories;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class DeadlinesRepository
 {
@@ -10,6 +11,13 @@ class DeadlinesRepository
     {
         return DB::table('charon_deadline')
             ->where('charon_id', $charonId)
+            ->delete();
+    }
+    public function deleteAllCalendarEventsForCharon($charonId)
+    {
+        Log::alert("Attempting to delete all charon events");
+        return DB::table('event')
+            ->where('instance', $charonId)
             ->delete();
     }
 }
