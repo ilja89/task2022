3eccb5cd99d13ecaa071367fc556b1e51f17347b
Karl Erik Õunapuu <karlerik@kreative.ee>
2019-10-09 09:44:51 +0300

    Fix Charon database mysql port

--- plugin/config/database.php
+++ plugin/config/database.php
@@ -44,7 +44,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', $CFG->dbhost),
-            'port' => env('DB_PORT', '3307'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', $CFG->dbname),
             'username' => env('DB_USERNAME', $CFG->dbuser),
             'password' => env('DB_PASSWORD', $CFG->dbpass),
