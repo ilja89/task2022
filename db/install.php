3153e44ef3e7e897ee4b3b1d46296a5dac0cf605
envomp <envomp@ttu.ee>
2020-06-18 20:45:51 +0300

    #101 create artifact

--- db/install.php
+++ db/install.php
@@ -25,29 +25,29 @@ function xmldb_charon_install()
 
     echo "</pre>";
 
-    if (!function_exists('apache_get_modules')) {
-        charon_installation_error("This plugin needs apache to redirect requests.");
-    }
+//    if (!function_exists('apache_get_modules')) {
+//        charon_installation_error("This plugin needs apache to redirect requests.");
+//    }
 
-    if (!in_array('mod_rewrite', apache_get_modules())) {
-        charon_installation_error("Please enable mod_rewrite using the following command: sudo a2enmod rewrite");
-    }
+//    if (!in_array('mod_rewrite', apache_get_modules())) {
+//        charon_installation_error("Please enable mod_rewrite using the following command: sudo a2enmod rewrite");
+//    }
 
-    if (!is_writable(__DIR__ . "/../") || !is_writable(__DIR__ . "/../plugin/storage/logs/")) {
-        charon_installation_error("mod/charon folder must be writable.");
-    }
+//    if (!is_writable(__DIR__ . "/../") || !is_writable(__DIR__ . "/../plugin/storage/logs/")) {
+//        charon_installation_error("mod/charon folder must be writable.");
+//    }
 
-    if (file_put_contents($charon_path . "composer-installer.php", fopen("https://getcomposer.org/installer", 'r'))) {
-        echo "Downloaded composer installer...\n";
-    } else {
-        charon_installation_error("Failed to download composer installer.");
-    }
+//    if (file_put_contents($charon_path . "composer-installer.php", fopen("https://getcomposer.org/installer", 'r'))) {
+//        echo "Downloaded composer installer...\n";
+//    } else {
+//        charon_installation_error("Failed to download composer installer.");
+//    }
 
-    if (!charon_command_exists("php")) {
-        charon_installation_error("Couldn't find 'php' in the system, please check that you have php cli installed.");
-    }
+//    if (!charon_command_exists("php")) {
+//        charon_installation_error("Couldn't find 'php' in the system, please check that you have php cli installed.");
+//    }
 
-    try_install($charon_path);
+//    try_install($charon_path);
 
     try_seed_database();
 
@@ -56,42 +56,42 @@ function xmldb_charon_install()
     return true;
 }
 
-/**
- * @param string $charon_path
- */
-function try_install(string $charon_path)
-{
-    try {
-        $composerInstall = "COMPOSER_HOME=\"" . $charon_path . "\" php " . $charon_path . "composer-installer.php --install-dir=" . $charon_path;
-
-        if (charon_is_function_available("exec")) {
-            echo exec($composerInstall) . "\n";
-        } else if (charon_is_function_available("shell_exec")) {
-            echo shell_exec($composerInstall) . "\n";
-        } else {
-            charon_installation_error("Command 'exec' must be available to install this plugin.");
-        }
-
-        require_once "phar://" . $charon_path . "composer.phar/src/bootstrap.php";
-        chdir($charon_path);
-        putenv("COMPOSER_HOME={$charon_path}");
-        putenv("COMPOSER={$charon_path}composer.json");
-        putenv("COMPOSER_VENDOR_DIR={$charon_path}vendor");
-        putenv("OSTYPE=OS400");
-        $app = new \Composer\Console\Application();
-        $factory = new \Composer\Factory();
-        $output = $factory->createOutput();
-        $input = new \Symfony\Component\Console\Input\ArrayInput(array(
-            'command' => 'install',
-            '--no-progress' => true,
-            '--no-dev' => true
-        ));
-        $input->setInteractive(false);
-        $cmdret = $app->doRun($input, $output); //unfortunately ->run() call exit() so we use doRun()
-    } catch (exception $e) {
-        echo $e->getMessage();
-    }
-}
+///**
+// * @param string $charon_path
+// */
+//function try_install(string $charon_path)
+//{
+//    try {
+//        $composerInstall = "COMPOSER_HOME=\"" . $charon_path . "\" php " . $charon_path . "composer-installer.php --install-dir=" . $charon_path;
+//
+//        if (charon_is_function_available("exec")) {
+//            echo exec($composerInstall) . "\n";
+//        } else if (charon_is_function_available("shell_exec")) {
+//            echo shell_exec($composerInstall) . "\n";
+//        } else {
+//            charon_installation_error("Command 'exec' must be available to install this plugin.");
+//        }
+//
+//        require_once "phar://" . $charon_path . "composer.phar/src/bootstrap.php";
+//        chdir($charon_path);
+//        putenv("COMPOSER_HOME={$charon_path}");
+//        putenv("COMPOSER={$charon_path}composer.json");
+//        putenv("COMPOSER_VENDOR_DIR={$charon_path}vendor");
+//        putenv("OSTYPE=OS400");
+//        $app = new \Composer\Console\Application();
+//        $factory = new \Composer\Factory();
+//        $output = $factory->createOutput();
+//        $input = new \Symfony\Component\Console\Input\ArrayInput(array(
+//            'command' => 'install',
+//            '--no-progress' => true,
+//            '--no-dev' => true
+//        ));
+//        $input->setInteractive(false);
+//        $cmdret = $app->doRun($input, $output); //unfortunately ->run() call exit() so we use doRun()
+//    } catch (exception $e) {
+//        echo $e->getMessage();
+//    }
+//}
 
 /**
  * @param string $charon_path
@@ -134,70 +134,70 @@ function try_seed_database()
     }
 }
 
-if (!function_exists("charon_command_exists")) {
-    /**
-     * Determines if a command exists on the current environment
-     *
-     * @param string $command The command to check
-     * @return bool True if the command has been found ; otherwise, false.
-     */
-    function charon_command_exists($command)
-    {
-        $whereIsCommand = (PHP_OS == 'WINNT') ? 'where' : 'which';
-
-        $process = proc_open(
-            "$whereIsCommand $command",
-            array(
-                0 => array("pipe", "r"), //STDIN
-                1 => array("pipe", "w"), //STDOUT
-                2 => array("pipe", "w"), //STDERR
-            ),
-            $pipes
-        );
-        if ($process !== false) {
-            $stdout = stream_get_contents($pipes[1]);
-            $stderr = stream_get_contents($pipes[2]);
-            fclose($pipes[1]);
-            fclose($pipes[2]);
-            proc_close($process);
-
-            return $stdout != '';
-        }
-
-        return false;
-    }
-}
-
-if (!function_exists("charon_is_function_available")) {
-    /**
-     * Checks if function is disabled or available
-     *
-     * @param string $command
-     * @return bool - true if available
-     */
-    function charon_is_function_available($command)
-    {
-        static $available;
-
-        if (!isset($available)) {
-            $available = true;
-            if (ini_get('safe_mode')) {
-                $available = false;
-            } else {
-                $d = ini_get('disable_functions');
-                $s = ini_get('suhosin.executor.func.blacklist');
-                if ("$d$s") {
-                    $array = preg_split('/,\s*/', "$d,$s");
-                    if (in_array($command, $array)) {
-                        $available = false;
-                    }
-                }
-            }
-        }
-
-        return $available;
-    }
-}
+//if (!function_exists("charon_command_exists")) {
+//    /**
+//     * Determines if a command exists on the current environment
+//     *
+//     * @param string $command The command to check
+//     * @return bool True if the command has been found ; otherwise, false.
+//     */
+//    function charon_command_exists($command)
+//    {
+//        $whereIsCommand = (PHP_OS == 'WINNT') ? 'where' : 'which';
+//
+//        $process = proc_open(
+//            "$whereIsCommand $command",
+//            array(
+//                0 => array("pipe", "r"), //STDIN
+//                1 => array("pipe", "w"), //STDOUT
+//                2 => array("pipe", "w"), //STDERR
+//            ),
+//            $pipes
+//        );
+//        if ($process !== false) {
+//            $stdout = stream_get_contents($pipes[1]);
+//            $stderr = stream_get_contents($pipes[2]);
+//            fclose($pipes[1]);
+//            fclose($pipes[2]);
+//            proc_close($process);
+//
+//            return $stdout != '';
+//        }
+//
+//        return false;
+//    }
+//}
+//
+//if (!function_exists("charon_is_function_available")) {
+//    /**
+//     * Checks if function is disabled or available
+//     *
+//     * @param string $command
+//     * @return bool - true if available
+//     */
+//    function charon_is_function_available($command)
+//    {
+//        static $available;
+//
+//        if (!isset($available)) {
+//            $available = true;
+//            if (ini_get('safe_mode')) {
+//                $available = false;
+//            } else {
+//                $d = ini_get('disable_functions');
+//                $s = ini_get('suhosin.executor.func.blacklist');
+//                if ("$d$s") {
+//                    $array = preg_split('/,\s*/', "$d,$s");
+//                    if (in_array($command, $array)) {
+//                        $available = false;
+//                    }
+//                }
+//            }
+//        }
+//
+//        return $available;
+//    }
+//}
 
 if (!function_exists("charon_remove_directory")) {
     function charon_remove_directory($dir)
