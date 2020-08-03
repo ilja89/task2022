1d2eecc259f29a5566c932eed24081eccdf64646
Kadri Männimets <kamann@ttu.ee>
2020-08-03 21:50:32 +0300

    157 view created.

---
+++ plugin/resources/assets/js/pages/popup/pages/DefenseRegistrationPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <page-title :title="'Defense registrations'"></page-title>
+        <defense-registrations-section></defense-registrations-section>
+    </div>
+</template>
+
+<script>
+
+    import PageTitle from "../partials/PageTitle";
+    import DefenseRegistrationsSection from "../sections/DefenseRegistrationsSection";
+
+    export default {
+        name: "defense-registrations-page",
+        components: { PageTitle, DefenseRegistrationsSection }
+
+    }
+</script>
