e4353200a368dc88572bda6460981a724e848485
envomp <envomp@ttu.ee>
2020-08-22 19:46:33 +0300

    Add token when sending to arete

--- plugin/app/Services/HttpCommunicationService.php
+++ plugin/app/Services/HttpCommunicationService.php
@@ -63,7 +63,7 @@ class HttpCommunicationService
         try {
             $client->request(
                 $method, $testerUrl,
-                ['headers' => ['Authorization' => 'charon ' . $testerToken], 'json' => $data]
+                ['headers' => ['Authorization' => 'X-Testing-Token ' . $testerToken], 'json' => $data]
             );
         } catch (RequestException $e) {
             Log::error(
