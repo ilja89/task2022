a708574456b71084c1176fe17067cbd8aed60272
envomp <envomp@ttu.ee>
2020-08-27 22:23:56 +0300

    add comment

--- db/upgrade.php
+++ db/upgrade.php
@@ -417,9 +417,15 @@ function xmldb_charon_upgrade($oldversion = 0)
     }
 
     if ($oldversion < 2020082701) {
-        $sql = "ALTER TABLE mdl_charon ADD COLUMN defense_threshold BIGINT(10) DEFAULT 50";
-        $DB->execute($sql);
+        try {
+            $sql = "ALTER TABLE mdl_charon ADD COLUMN defense_threshold BIGINT(10) DEFAULT 50";
+            $DB->execute($sql);
+        } catch (dml_write_exception $e) {
+            // Ignored intentionally
+        }
     }
+
+    // FROM THIS POINT ON PLEASE USE $CFG->prefix instead of `mdl_` as prefix !!!
 
     return true;
 }
