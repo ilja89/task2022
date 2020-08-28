9e82f8e02c37f08cdaf2eda260b7197f002b14e4
Kadri Männimets <kamann@ttu.ee>
2020-08-29 02:42:00 +0300

    F.

--- plugin/app/Http/Middleware/RequireCharonManaging.php
+++ plugin/app/Http/Middleware/RequireCharonManaging.php
@@ -49,6 +49,7 @@ class RequireCharonManaging
         Log::error($request);
         Log::error($request->route('charon'));
         Log::error('wat???');
+        Log::error(gettype($request->route('charon')));
         //Log::error(get_class($request->route('charon')));
         if (is_int($request->route('charon'))) {
             Log::error('int found');
