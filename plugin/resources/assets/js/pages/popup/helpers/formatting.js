cc766b67f06ecad6194897273917e65e56b456ee
envomp <envomp@ttu.ee>
2020-09-01 23:26:07 +0300

    formatting

--- plugin/resources/assets/js/pages/popup/helpers/formatting.js
+++ plugin/resources/assets/js/pages/popup/helpers/formatting.js
@@ -27,7 +27,7 @@ export const formatSubmissionResults = (submission, separator = ' | ') => submis
  * @returns {String}
  */
 export const formatDeadline = deadline => {
-    const date = deadline.deadline_time // TODO
+    const date = new Date(deadline.deadline_time).toLocaleString()
     const percentage = deadline.percentage
     const groupName = deadline.group
         ? deadline.group.name
