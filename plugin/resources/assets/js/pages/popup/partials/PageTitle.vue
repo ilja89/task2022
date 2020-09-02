f61fa69604da64662e73849946136bf0a8b14c93
envomp <envomp@ttu.ee>
2020-09-02 14:06:25 +0300

    Students in group should be shown only when hovering

--- plugin/resources/assets/js/pages/popup/partials/PageTitle.vue
+++ plugin/resources/assets/js/pages/popup/partials/PageTitle.vue
@@ -26,6 +26,29 @@
                     v-for="member in group.members"
                     v-bind:key="member.idnumber"
                   >{{member.firstname}} {{member.lastname}} ({{member.idnumber}})</p>
+
+                  <v-bottom-sheet v-model="sheet" inset>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                              color="orange"
+                              dark
+                              v-bind="attrs"
+                              v-on="on"
+                      >
+                        Open Inset
+                      </v-btn>
+                    </template>
+                    <v-sheet class="text-center" height="200px">
+                      <v-btn
+                              class="mt-6"
+                              text
+                              color="error"
+                              @click="sheet = !sheet"
+                      >close</v-btn>
+                      <div class="my-3">This is a bottom sheet using the inset prop</div>
+                    </v-sheet>
+                  </v-bottom-sheet>
+
                 </div>
               </tippy>
             </div>
@@ -41,6 +64,9 @@ import { mapState, mapGetters } from "vuex";
 import VueTippy, { TippyComponent } from "vue-tippy";
 export default {
   components: { TippyComponent },
+  data: () => ({
+    sheet: false,
+  }),
   props: {
     title: {
       required: false,
