3a708f7e07241e508ea4c8cf34be8e8f31c2c4d6
envomp <envomp@ttu.ee>
2020-09-02 14:26:11 +0300

    Enable vuetify

--- plugin/resources/assets/js/pages/popup/popup.js
+++ plugin/resources/assets/js/pages/popup/popup.js
@@ -1,7 +1,6 @@
 import '../../bootstrap'
 import Vue from 'vue'
 import axios from 'axios'
-import vuetify from 'vuetify'
 import Popup from './Popup.vue'
 import router from './routes'
 import store from './store'
@@ -11,6 +10,7 @@ import VueApexCharts from 'vue-apexcharts';
 import VueGoodTablePlugin from 'vue-good-table';
 import 'vue-good-table/dist/vue-good-table.css';
 import VueJsonToCsv from 'vue-json-to-csv';
+import Vuetify from "vuetify";
 
 Vue.directive('highlightjs', HighlightDirective);
 
@@ -18,15 +18,13 @@ Vue.component('apexcharts', VueApexCharts);
 
 Vue.use(VueGoodTablePlugin);
 
-Vue.use(vuetify)
-
 Vue.component('vue-json-to-csv', VueJsonToCsv);
 
 window.axiosNoLoading = axios.create();
 
 const app = new Vue({
     el: '#app',
-    vuetify,
+    vuetify: new Vuetify(),
 
     components: {Popup},
 
