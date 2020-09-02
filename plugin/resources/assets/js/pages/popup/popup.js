90d8c69737be6eb26e0f9d8fed2eb0abc9f4b7d3
envomp <envomp@ttu.ee>
2020-09-02 14:23:55 +0300

    Enable vuetify

--- plugin/resources/assets/js/pages/popup/popup.js
+++ plugin/resources/assets/js/pages/popup/popup.js
@@ -1,12 +1,12 @@
 import '../../bootstrap'
 import Vue from 'vue'
 import axios from 'axios'
-
+import vuetify from 'vuetify'
 import Popup from './Popup.vue'
 import router from './routes'
 import store from './store'
 
-import { HighlightDirective } from './../../directives';
+import {HighlightDirective} from './../../directives';
 import VueApexCharts from 'vue-apexcharts';
 import VueGoodTablePlugin from 'vue-good-table';
 import 'vue-good-table/dist/vue-good-table.css';
@@ -18,18 +18,20 @@ Vue.component('apexcharts', VueApexCharts);
 
 Vue.use(VueGoodTablePlugin);
 
+Vue.use(vuetify)
+
 Vue.component('vue-json-to-csv', VueJsonToCsv);
 
 window.axiosNoLoading = axios.create();
 
 const app = new Vue({
     el: '#app',
+    vuetify,
 
-
-    components: { Popup },
+    components: {Popup},
 
     mounted() {
-        this.$store.dispatch('initializeCourse', { courseId: window.course_id })
+        this.$store.dispatch('initializeCourse', {courseId: window.course_id})
     },
 
     router,
