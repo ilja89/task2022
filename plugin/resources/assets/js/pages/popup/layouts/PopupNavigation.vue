be97f4a7b380f40ba6fbec1579ce0882ab7d68ea
envomp <envomp@ttu.ee>
2020-09-03 22:16:09 +0300

    search

--- plugin/resources/assets/js/pages/popup/layouts/PopupNavigation.vue
+++ plugin/resources/assets/js/pages/popup/layouts/PopupNavigation.vue
@@ -19,6 +19,7 @@
 
                 <v-list-item-content>
                     <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    {{item.title}}}
                 </v-list-item-content>
             </v-list-item>
         </v-list>
