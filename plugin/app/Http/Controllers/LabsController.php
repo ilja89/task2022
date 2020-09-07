12b32bc84d121b44cc367e5bb87f00c62b193c3c
envomp <envomp@ttu.ee>
2020-09-08 01:39:24 +0300

    defence page not showing todays registrations

--- plugin/app/Http/Controllers/LabsController.php
+++ plugin/app/Http/Controllers/LabsController.php
@@ -6,9 +6,11 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use TTU\Charon\Models\Lab;
 
-class LabsController extends Controller {
+class LabsController extends Controller
+{
 
-    public function insert(Request $request) {
+    public function insert(Request $request)
+    {
         $lab = new Lab;
         $lab->start = $request->input('start')['time'];
         $lab->end = $request->input('end')['time'];
@@ -27,13 +29,14 @@ class LabsController extends Controller {
         return $labs;
     }
 
-    public function findLabsByCharonLaterEqualToday(Request $request) {
+    public function findLabsByCharonLaterEqualToday(Request $request)
+    {
         $charonId = $request->input('id');
 
         return \DB::table('charon_lab')  // id, start, end
         ->join('charon_defense_lab', 'charon_defense_lab.lab_id', 'charon_lab.id') // id, lab_id, charon_id
         ->where('charon_id', $charonId)
-            ->whereDate('start', '>=', Carbon::now())
+            ->whereDate('start', '>=', Carbon::now()->toDateTime())
             ->select('charon_defense_lab.id', 'start', 'end', 'course_id')
             ->get();
     }
