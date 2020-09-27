acd5f0657704d74b7e06f12ad916eae554985db8
envomp <envomp@ttu.ee>
2020-09-27 15:16:48 +0300

    add keys #238

--- plugin/resources/assets/js/pages/popup/sections/SubmissionOverviewSection.vue
+++ plugin/resources/assets/js/pages/popup/sections/SubmissionOverviewSection.vue
@@ -1,8 +1,8 @@
 <template>
-    {{submission}}
     <popup-section :title="activeCharonName" :subtitle="submissionOrderNrText" :key="submission.id">
         <template slot="header-right">
             <span v-if="charon_confirmed_points !== null" class="extra-info-text">
+                {{submission}}
                 Current points: {{ charon_confirmed_points }}p
             </span>
             <v-btn :disabled="saveIsDisabled" class="ma-2" tile outlined color="primary" @click="saveSubmission">
