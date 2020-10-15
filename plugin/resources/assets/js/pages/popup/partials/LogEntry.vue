7ad9867af3255736c05b89d900c26a2de8cb3d3c
Jaak Kütt <jaak.kytt@gmail.com>
2020-10-15 21:48:23 +0300

    #291 Component for individual log entries

---
+++ plugin/resources/assets/js/pages/popup/partials/LogEntry.vue
@@ -0,0 +1,57 @@
+<template>
+
+    <v-list-group
+      v-if="Array.isArray(children)"
+      :key="title"
+      :prepend-icon=icon
+      no-action
+    >
+        <template v-slot:activator>
+            <v-list-item-content>
+                <v-list-item-title v-text="title"></v-list-item-title>
+            </v-list-item-content>
+        </template>
+
+        <v-list-item v-for="child in children" :key="child">
+            <v-list-item-content>
+                <v-list-item-title v-text="child"></v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+    </v-list-group>
+
+    <v-list-item v-else :key="title" :prepend-icon=icon>
+        <v-list-item-title v-text="title"></v-list-item-title>
+    </v-list-item>
+
+</template>
+
+<script>
+export default {
+    props: {
+        log: Array
+    },
+    computed: {
+        title: function () {
+            return this.log[0];
+        },
+        children: function () {
+            if (this.log.length === 1) {
+                return false;
+            }
+            return this.log.slice(1);
+        },
+        icon: function () {
+            let level = this.log[0].split(':')[2].split('.')[1];
+            if (level === 'WARNING') {
+                return 'mdi-alert-outline';
+            }
+
+            if (level === 'ERROR' || level === 'CRITICAL') {
+                return 'mdi-alert-octagram';
+            }
+
+            return 'mdi-alert-circle-outline';
+        }
+    }
+}
+</script>
