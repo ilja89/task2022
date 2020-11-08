db3bb63f003aff04c300fd8326a496c1c0895599
Jaak Kütt <jaak.kytt@gmail.com>
2020-11-08 12:20:11 +0200

    #315 Refactor GitCallbackController to cover more cases when info is sent to tester

--- plugin/app/Http/Controllers/Api/GitCallbackController.php
+++ plugin/app/Http/Controllers/Api/GitCallbackController.php
@@ -4,17 +4,13 @@ namespace TTU\Charon\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\DB;
 use TTU\Charon\Events\GitCallbackReceived;
 use TTU\Charon\Http\Controllers\Controller;
 use TTU\Charon\Http\Requests\GitCallbackPostRequest;
 use TTU\Charon\Http\Requests\GitCallbackRequest;
 use TTU\Charon\Repositories\GitCallbacksRepository;
 use TTU\Charon\Repositories\CourseSettingsRepository;
-use Zeizig\Moodle\Models\Course;
-use Zeizig\Moodle\Models\User;
-use Zeizig\Moodle\Models\Grouping;
-use TTU\Charon\Models\Charon;
+use TTU\Charon\Services\GitCallbackService;
 
 /**
  * Class GitCallbackController.
@@ -30,22 +26,28 @@ class GitCallbackController extends Controller
     /** @var CourseSettingsRepository */
     private $courseSettingsRepository;
 
+    /** @var GitCallbackService */
+    private $gitCallbackService;
+
     /**
      * GitCallbackController constructor.
      *
      * @param Request $request
      * @param GitCallbacksRepository $gitCallbacksRepository
      * @param CourseSettingsRepository $courseSettingsRepository
+     * @param GitCallbackService $gitCallbackService
      */
     public function __construct(
         Request $request,
         GitCallbacksRepository $gitCallbacksRepository,
-        CourseSettingsRepository $courseSettingsRepository
+        CourseSettingsRepository $courseSettingsRepository,
+        GitCallbackService $gitCallbackService
     )
     {
         parent::__construct($request);
         $this->gitCallbacksRepository = $gitCallbacksRepository;
         $this->courseSettingsRepository = $courseSettingsRepository;
+        $this->gitCallbackService = $gitCallbackService;
     }
 
     /**
@@ -88,151 +90,89 @@ class GitCallbackController extends Controller
      */
     public function indexPost(GitCallbackPostRequest $request)
     {
-
         $repo = $request->input('repository')['git_ssh_url'];
-        $initial_user = $request->input('user_username');
+        $initialUser = $request->input('user_username');
+        $callbackUrl = $request->getUriForPath('/api/tester_callback');
+        $fullUrl = $request->fullUrl();
+        $params = [];
 
-        Log::info('Initial user has username: "' . $initial_user . '"');
-        // Fetch Course name and Project folder from Git repo address
-        $meta = str_replace('.git', '', substr($repo, strrpos($repo, '/') + 1));
+        Log::debug('Initial user has username: "' . $initialUser . '"');
 
-        // try course with full name (no grouping)
-        $course = Course::where('shortname', $meta)->first();
-        $course_name = $meta;
+        if ($request->input('commits')) {
+            $params['email'] = $request->input('commits.0.author.email');
+        }
+
+        $course = $this->gitCallbackService->getCourse($repo);
 
         if (!$course) {
-            // try to split COURSE-PROJECT
-            $pos = strrpos($meta, "-");
-            while ($pos !== false) {
-                $course_name = substr($meta, 0, $pos);
-                $project_folder = substr($meta, $pos + 1);
-                Log::info('Looking for course"' . $course_name . '" and project "' . $project_folder . '"');
-                $course = Course::where('shortname', $course_name)->first();
-                if ($course) {
-                    Log::info("Course found!");
-                    break;
-                }
-                // find the next place to split the rest of the name
-                $pos = strrpos($course_name, "-");
-            }
+            Log::warning('No course discovered, maybe git repo address is not in valid format.');
+
+            $this->gitCallbackService->saveCallbackForUser($initialUser, $fullUrl, $repo, $callbackUrl, $params);
+
+            return "SUCCESS";
         }
 
-        $gitTestSource = $this->courseSettingsRepository->getCourseSettingsByCourseId($course->id)->unittests_git;
-        $testingPlatform = $this->courseSettingsRepository->getCourseSettingsByCourseId($course->id)->testerType->name;
-        $usernames = array();
-        $charon = null;
+        Log::debug('Found course: "' . $course->shortname . '" with ID ' . $course->id);
 
-        if (isset($project_folder)) {
-            Log::info('Discovered course name: "' . $course_name . '" and project folder: "' . $project_folder . '"');
-        } else {
-            Log::info('Discovered course name: "' . $course_name . '" but no project folder.');
+        $params['gitTestRepo'] = $this->courseSettingsRepository->getCourseSettingsByCourseId($course->id)->unittests_git;
+        $params['testingPlatform'] = $this->courseSettingsRepository->getCourseSettingsByCourseId($course->id)->testerType->name;
+
+        $modifiedFiles = $this->gitCallbackService->getModifiedFiles($request->get('commits'));
+        $charons = $this->gitCallbackService->findCharons($modifiedFiles, $course->id);
+
+        if (empty($charons)) {
+            Log::warning('This charon is not a group work. Forwarding to tester.');
+
+            $this->gitCallbackService->saveCallbackForUser($initialUser, $fullUrl, $repo, $callbackUrl, $params);
+
+            return "SUCCESS";
         }
 
-        if (!$course) {
-            Log::info('No course discovered, maybe git repo address is not in valid format.');
-            if (!in_array($request->input('user_username'), $usernames)) {
-                array_push($usernames, $request->input('user_username'));
+        foreach ($charons as $charon) {
+            Log::debug("Found charon with id: " . $charon->id);
+
+            if ($charon['tester_extra'] != null) {
+                $params['dockerExtra'] = $charon['tester_extra'];
             }
-        } else {
-            Log::info("Found course. It's id is " . $course->id);
-            if (!isset($project_folder)) {
-                Log::info('This charon is not a group work. Forwarding to tester.');
-                if (!in_array($request->input('user_username'), $usernames)) {
-                    array_push($usernames, $request->input('user_username'));
-                }
-            } else {
-                // Find charon
-                $charon = Charon::where([
-                    ['project_folder', $project_folder],
-                    ['course', $course->id]])->first();
-
-                Log::info("Found charon with id: " . $charon->id);
-
-                // TODO: Trim model requests to select only required fields
-                if ($charon->grouping_id !== null) {
-                    Log::info('Charon has grouping id ' . $charon->grouping_id);
-                    // Get grouping
-                    $grouping = Grouping::where('id', $charon->grouping_id)->first();
-                    // Get submitter's User ID by username
-                    Log::info('Trying to get ID of user "' . $initial_user . '"');
-                    $initiator = User::where('username', $initial_user . "@ttu.ee")->first()->id;
-                    //Log::info('User object is: ' . $initiator);
-                    //Log::info('Initiator ID is: ' . $initiator);
-                    Log::info('Initiator ID is: ' . $initiator);
-                    // Get groups of submitter
-                    $initiator_groups = DB::table('groups_members')->select('groupid')->where('userid', $initiator)->get();
-
-                    foreach ($initiator_groups as $group) {
-                        // Select groups that are in chosen Charon's grouping
-                        $grouping_group = $grouping->groups()->where('groups.id', $group->groupid)->first();
-                        if ($grouping_group) {
-                            // Fetch usernames from group
-                            Log::info('Grouping group' . $grouping_group->name);
-                            $members = $grouping_group->members()->get();
-                            foreach ($members as $member) {
-                                if (!in_array($member->username, $usernames)) {
-                                    array_push($usernames, $member->username);
-                                }
-                            }
-                            break;
-                        }
-                    }
-                } else {
-                    Log::info('This charon is not a group work or is broken. Forwarding to tester.');
-                    if (!in_array($request->input('user_username'), $usernames)) {
-                        array_push($usernames, $request->input('user_username'));
-                    }
-                }
+            if ($charon['docker_test_root'] != null) {
+                $params['dockerTestRoot'] = $charon['docker_test_root'];
             }
-        }
-
-        foreach ($usernames as $username) {
-            $username = str_replace('@ttu.ee', '', $username);
-            Log::info('Submitting work as user "' . $username . '"');
-            $gitCallback = $this->gitCallbacksRepository->save(
-                $request->fullUrl(),
-                $repo,
-                $username
-            );
-
-            $params = [
-                'uniid' => $username,
-                'gitStudentRepo' => $repo,
-                'testingPlatform' => $testingPlatform,
-                'gitTestRepo' => $gitTestSource,
-                'email' => $username . "@ttu.ee"
-            ];
-
-            if ($request->input('commits')) {
-                $params['email'] = $request->input('commits.0.author.email');
+            if ($charon['docker_content_root'] != null) {
+                $params['dockerContentRoot'] = $charon['docker_content_root'];
+            }
+            if ($charon['docker_timeout'] != null) {
+                $params['dockerTimeout'] = $charon['docker_timeout'];
+            }
+            if ($charon['tester_type_code'] != null) {
+                $params['testingPlatform'] = $charon->testerType->name;
+            }
+            if ($charon['system_extra'] != null) {
+                $params['systemExtra'] = explode(',', $charon['system_extra']);
             }
 
-            if ($charon != null) {
-                if ($charon['tester_extra'] != null) {
-                    $params['dockerExtra'] = $charon['tester_extra'];
-                }
-                if ($charon['docker_test_root'] != null) {
-                    $params['dockerTestRoot'] = $charon['docker_test_root'];
-                }
-                if ($charon['docker_content_root'] != null) {
-                    $params['dockerContentRoot'] = $charon['docker_content_root'];
-                }
-                if ($charon['docker_timeout'] != null) {
-                    $params['dockerTimeout'] = $charon['docker_timeout'];
-                }
-                if ($charon['tester_type_code'] != null) {
-                    $params['testingPlatform'] = $charon->testerType->name;
-                }
-                if ($charon['system_extra'] != null) {
-                    $params['systemExtra'] = explode(',', $charon['system_extra']);
-                }
+            if ($charon->grouping_id == null) {
+                Log::info('This charon is not a group work or is broken. Forwarding to tester.');
+
+                $this->gitCallbackService->saveCallbackForUser($initialUser, $fullUrl, $repo, $callbackUrl, $params);
+
+                return "SUCCESS";
             }
 
-            event(new GitCallbackReceived(
-                $gitCallback,
-                $request->getUriForPath('/api/tester_callback'),
-                $params
-            ));
+            Log::debug('Charon has grouping id ' . $charon->grouping_id);
+
+            $usernames = $this->gitCallbackService->getGroupUsers($charon->grouping_id, $initialUser);
+
+            if (empty($usernames)) {
+                Log::warning('Unable to find users in group. Forwarding to tester.');
+
+                $this->gitCallbackService->saveCallbackForUser($initialUser, $fullUrl, $repo, $callbackUrl, $params);
+
+                return "SUCCESS";
+            }
+
+            foreach ($usernames as $username) {
+                $this->gitCallbackService->saveCallbackForUser($username, $fullUrl, $repo, $callbackUrl, $params);
+            }
         }
 
         return "SUCCESS";
--- plugin/app/Services/GitCallbackService.php
+++ plugin/app/Services/GitCallbackService.php
@@ -1,19 +1,26 @@
 <?php
 
-
 namespace TTU\Charon\Services;
 
-
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use TTU\Charon\Events\GitCallbackReceived;
 use TTU\Charon\Exceptions\IncorrectSecretTokenException;
+use TTU\Charon\Models\Charon;
 use TTU\Charon\Models\GitCallback;
 use TTU\Charon\Repositories\GitCallbacksRepository;
+use Zeizig\Moodle\Models\Course;
+use Zeizig\Moodle\Models\Grouping;
+use Zeizig\Moodle\Models\User;
 
 class GitCallbackService
 {
+    const COMMIT_ACTION_TYPES = ['added', 'modified', 'removed'];
+    const DEFAULT_EMAIL_SUFFIX = '@ttu.ee';
+
     /** @var GitCallbacksRepository */
     private $gitCallbacksRepository;
-
 
     /**
      * GitCallbackService constructor.
@@ -26,13 +33,187 @@ class GitCallbackService
     }
 
     /**
+     * Try to identify course from repository url
+     *
+     * @param string $repository
+     * @return Course
+     */
+    public function getCourse(string $repository)
+    {
+        // Fetch Course name and Project folder from Git repo address
+        $meta = str_replace('.git', '', substr($repository, strrpos($repository, '/') + 1));
+
+        // Try course with full name (no grouping)
+        $course = Course::where('shortname', $meta)->first();
+        if ($course) {
+            return $course;
+        }
+
+        // Try to split COURSE-PROJECT
+        $pos = strrpos($meta, "-");
+        while ($pos !== false) {
+            $course_name = substr($meta, 0, $pos);
+            $project_folder = substr($meta, $pos + 1);
+            Log::debug('Looking for course "' . $course_name . '" and project "' . $project_folder . '"');
+            $course = Course::where('shortname', $course_name)->first();
+            if ($course) {
+                Log::debug("Course found!");
+                return $course;
+            }
+            // Find the next place to split the rest of the name
+            $pos = strrpos($course_name, "-");
+        }
+
+        return null;
+    }
+
+    /**
+     * Extract all the files touched by given commits
+     *
+     * @param array $commits
+     * @return array
+     */
+    public function getModifiedFiles(array $commits) {
+        if (empty($commits)) {
+            return [];
+        }
+
+        return array_reduce($commits, function ($result, $commit) {
+            foreach (self::COMMIT_ACTION_TYPES as $type) {
+                if (isset($commit[$type]) && is_array($commit[$type])) {
+                    $result = array_merge($result, $commit[$type]);
+                }
+            }
+            return $result;
+        });
+    }
+
+    /**
+     * Find charons involved in callback by identifying project charon project_folder-s in the file path
+     *
+     * Treat / and \ as same for folder naming purposes
+     *
+     * @param array $modifiedFiles
+     * @param int $courseId
+     * @return Charon[]
+     */
+    public function findCharons(array $modifiedFiles, int $courseId) {
+        if (empty($modifiedFiles)) {
+            return [];
+        }
+
+        return Charon::where([['course', $courseId]])
+            ->filter(function ($charon) use ($modifiedFiles) {
+                foreach ($modifiedFiles as $file) {
+                    $file = str_replace('\\', '/', $file);
+                    $folder = str_replace('\\', '/', $charon->project_folder);
+                    if (substr($file, 0, strlen($folder)) === $folder) {
+                        return true;
+                    }
+                }
+                return false;
+            })->all();
+    }
+
+    /**
+     * Select groups that are in chosen Charon's grouping
+     *
+     * @param int $groupId
+     * @param string $initialUser
+     * @return array
+     */
+    public function getGroupUsers(int $groupId, string $initialUser) {
+        $grouping = Grouping::where('id', $groupId)->first();
+        if (!$grouping) {
+            Log::error('Unable to find group by ID ' . $groupId);
+            return [];
+        }
+
+        Log::debug('Trying to get ID of user "' . $initialUser . '"');
+
+        $initiator = User::where('username', $initialUser . self::DEFAULT_EMAIL_SUFFIX)->first();
+        if (!$initiator) {
+            Log::warning('Trying to find user "' . $initialUser . '"');
+            return [];
+        }
+
+        Log::debug('Initiator ID is: ' . $initiator->id);
+
+        $initiator_groups = DB::table('groups_members')
+            ->select('groupid')
+            ->where('userid', $initiator->id)
+            ->get();
+
+        $usernames = [];
+
+        foreach ($initiator_groups as $group) {
+            $grouping_group = $grouping->groups()->where('groups.id', $group->groupid)->first();
+            if (!$grouping_group) {
+                continue;
+            }
+
+            Log::debug('Grouping group ' . $grouping_group->name);
+            $members = $grouping_group->members()->get();
+            foreach ($members as $member) {
+                if (!in_array($member->username, $usernames)) {
+                    array_push($usernames, $member->username);
+                }
+            }
+            break;
+        }
+
+        return $usernames;
+    }
+
+    /**
+     * Save callback and send event for tester
+     *
+     * @param string $username
+     * @param string $fullUrl
+     * @param string $repositoryUrl
+     * @param string $callbackUrl
+     * @param array $params
+     */
+    public function saveCallbackForUser(
+        string $username,
+        string $fullUrl,
+        string $repositoryUrl,
+        string $callbackUrl,
+        array $params
+    ) {
+        $username = str_replace(self::DEFAULT_EMAIL_SUFFIX, '', $username);
+
+        Log::info('Submitting work as user "' . $username . '"');
+
+        $gitCallback = $this->gitCallbacksRepository->save(
+            $fullUrl,
+            $repositoryUrl,
+            $username
+        );
+
+        $params['uniid'] = $username;
+        $params['gitStudentRepo'] = $repositoryUrl;
+
+        if (!isset($params['email'])) {
+            $params['email'] = $username . self::DEFAULT_EMAIL_SUFFIX;
+        }
+
+        event(new GitCallbackReceived(
+            $gitCallback,
+            $callbackUrl,
+            $params
+        ));
+    }
+
+    /**
      * Check if the given token is valid and returns a Git callback.
      *
-     * @param  string  $token
+     * @param string $token
      *
      * @return GitCallback
+     * @throws IncorrectSecretTokenException
      */
-    public function checkGitCallbackForToken($token)
+    public function checkGitCallbackForToken(string $token)
     {
         $gitCallback = $this->gitCallbacksRepository->findByToken($token);
         $this->checkGitCallback($gitCallback);
