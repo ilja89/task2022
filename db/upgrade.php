1bec3e31e9687c812e21a3c887d11bb2f55278b4
Jaak Kütt <jaak.kytt@gmail.com>
2021-02-16 07:39:28 +0200

    #380 fix return statement, improve order of applied entries and version check

--- db/upgrade.php
+++ db/upgrade.php
@@ -575,8 +575,25 @@ function xmldb_charon_upgrade($oldversion = 0)
         );
     }
 
-    if ($oldversion < 2021020101) {
+    if ($oldversion < 2021020601) {
+        $table = new xmldb_table('charon_grademap');
+        $field = new xmldb_field('persistent', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, 0, null);
 
+        if (!$dbManager->field_exists($table, $field)) {
+            $dbManager->add_field($table, $field);
+        }
+    }
+
+    if ($oldversion < 2021021001) {
+        $table = new xmldb_table('charon_lab');
+        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '255', null, null, null, null, null);
+
+        if (!$dbManager->field_exists($table, $field)) {
+            $dbManager->add_field($table, $field);
+        }
+    }
+
+    if ($oldversion < 2021021601) {
         $sql = "CREATE TABLE " . $CFG->prefix . "charon_defense_registration(" .
             "    id BIGINT(10) AUTO_INCREMENT NOT NULL," .
             "    student_id BIGINT(10) NULL," .
@@ -611,27 +628,7 @@ function xmldb_charon_upgrade($oldversion = 0)
         if (!$dbManager->table_exists($table)) {
             $DB->execute($sql);
         }
-
     }
 
-    if ($oldversion < 2021020601) {
-        $table = new xmldb_table('charon_grademap');
-        $field = new xmldb_field('persistent', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, 0, null);
-
-        if (!$dbManager->field_exists($table, $field)) {
-            $dbManager->add_field($table, $field);
-        }
-    }
-
-    if ($oldversion < 2021021001) {
-        $table = new xmldb_table('charon_lab');
-        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '255', null, null, null, null, null);
-
-        if (!$dbManager->field_exists($table, $field)) {
-            $dbManager->add_field($table, $field);
-        }
-
-
-        return true;
-    }
+    return true;
 }
