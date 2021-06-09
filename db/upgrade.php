dc1a64da8640b49818c84f77f37620375bdb9193
Jüri Helekivi <juri.helekivi@ttu.ee>
2021-06-09 05:29:32 +0000

    Apply 2 suggestion(s) to 2 file(s)

--- db/upgrade.php
+++ db/upgrade.php
@@ -699,6 +699,7 @@ function xmldb_charon_upgrade($oldversion = 0)
             "    group_id BIGINT(10) NOT NULL," .
             "    PRIMARY KEY (id)," .
             "    INDEX IXFK_charon_lab_group_charon_lab (lab_id)," .
+            "    CONSTRAINT UQ_charon_lab_lab_and_group UNIQUE (lab_id, group_id)," .
             "    CONSTRAINT FK_charon_lab_group_charon_lab" .
             "        FOREIGN KEY (lab_id)" .
             "            REFERENCES " . $CFG->prefix . "charon_lab(id)" .
