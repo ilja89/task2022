f25b89fbd2cc23adff24a1f3ca1f2e06e99d3579
user one <userone@users-MBP.lan>
2021-07-13 20:06:11 +0300

    #431 potentially finalised for issue

--- plugin/resources/assets/js/pages/popup/sections/SubmissionsSection.vue
+++ plugin/resources/assets/js/pages/popup/sections/SubmissionsSection.vue
@@ -5,6 +5,9 @@
     >
 
         <template slot="header-right">
+            <v-spacer></v-spacer>
+            Total points: {{ totalCharonPoints }}
+            <v-spacer></v-spacer>
             <charon-select/>
         </template>
 
@@ -16,8 +19,59 @@
 <script>
     import { PopupSection } from '../layouts';
     import { CharonSelect, SubmissionsList } from '../partials';
+    import {mapState} from "vuex";
+    import {Charon} from "../../../api";
 
     export default {
         components: { PopupSection, CharonSelect, SubmissionsList },
+
+        data() {
+            return {
+                totalCharonPoints: null
+            }
+        },
+
+        computed: {
+            ...mapState([
+                'charon',
+                'student',
+            ]),
+        },
+
+        methods: {
+
+            refreshPoints() {
+                if (this.student == null || this.charon == null || this._inactive) {
+                    return
+                }
+
+                Charon.getResultForStudent(
+                    this.charon.id,
+                    this.student.id,
+                    points => {
+                        this.totalCharonPoints = points;
+                    }
+                );
+            },
+        },
+
+        watch: {
+            charon() {
+                this.refreshPoints()
+            },
+
+            student() {
+                this.refreshPoints()
+            },
+        },
+
+        created() {
+            this.refreshPoints()
+            VueEvent.$on('refresh-page', this.refreshPoints)
+        },
+
+        beforeDestroy() {
+            VueEvent.$off('refresh-page', this.refreshPoints)
+        },
     }
 </script>
