8f869e8690f0170d9f91db9dc3226a7ac37e19af
Artur-Aleksander Pärnoja <arthurp200148@gmail.com>
2021-07-13 17:34:07 +0300

    #474 Make submit all files at once and send all data to the back-end on the submit button clicked.

--- plugin/resources/assets/js/pages/assignment/components/AssignmentTab.vue
+++ plugin/resources/assets/js/pages/assignment/components/AssignmentTab.vue
@@ -4,17 +4,24 @@
       <charon-tab v-for="(code, index) in codes"
                   :name="code"
                   :selected="index===0">
-        <code-editor :code="code"
+        <code-editor :codeId="index"
+                     :code="code"
                      :language="language"
+                     :codes="codes"
         ></code-editor>
       </charon-tab>
     </charon-tabs>
+    <v-btn class="ma-2 submitBtn" small tile outlined color="primary" @click="submitClicked">
+      Submit
+    </v-btn>
   </div>
 </template>
 <script>
 import CharonTab from "../../../components/partials/CharonTab";
 import CharonTabs from "../../../components/partials/CharonTabs";
 import CodeEditor from "./CodeEditor";
+import Submission from "../../../api/Submission";
+
 export default {
   name: "AssignmentTab",
 
@@ -27,6 +34,25 @@ export default {
   data() {
     return {
       codes: ["def", "def2", "def3"]
+    }
+  },
+
+  methods: {
+
+    submitClicked() {
+      let sourceFiles = [];
+
+      for (let i = 0; i < this.codes.length; i++) {
+        sourceFiles.push({"path": "EX03", "content": this.codes[i]});
+      }
+
+      try {
+        Submission.saveSubmission(sourceFiles, window.charonId, window.studentId, () =>
+            VueEvent.$emit('show-notification', 'Submission successfully saved!')
+        )
+      } catch (e) {
+        VueEvent.$emit('show-notification', 'Error saving submission!')
+      }
     }
   }
 }
