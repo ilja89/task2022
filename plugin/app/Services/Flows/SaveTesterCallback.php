170015052c69554d6eada9bf6d635b3b9c510129
ilkuzn <ilkuzn@ttu.ee>
2021-07-16 12:39:43 +0000

    Update SaveTesterCallback.php

--- plugin/app/Services/Flows/SaveTesterCallback.php
+++ plugin/app/Services/Flows/SaveTesterCallback.php
@@ -10,11 +10,13 @@ use TTU\Charon\Http\Requests\TesterCallbackRequest;
 use TTU\Charon\Models\GitCallback;
 use TTU\Charon\Models\Submission;
 use TTU\Charon\Repositories\ResultRepository;
+use TTU\Charon\Repositories\StudentsRepository;
 use TTU\Charon\Services\CharonGradingService;
 use TTU\Charon\Services\SubmissionService;
 use TTU\Charon\Services\TestSuiteService;
 use Zeizig\Moodle\Models\User;
 use Zeizig\Moodle\Services\UserService;
+use Zeizig\Moodle\Models\Course;
 
 class SaveTesterCallback
 {
@@ -39,20 +41,89 @@ class SaveTesterCallback
      * @param UserService $userService
      * @param ResultRepository $resultRepository
      * @param TestSuiteService $testSuiteService
+     * @param course $course
+     * @param studentsRepository $studentsRepository
      */
     public function __construct(
         SubmissionService $submissionService,
         CharonGradingService $charonGradingService,
         UserService $userService,
         ResultRepository $resultRepository,
-        TestSuiteService $testSuiteService
+        TestSuiteService $testSuiteService,
+        Course $course,
+        StudentsRepository $studentsRepository
     ) {
         $this->submissionService = $submissionService;
         $this->charonGradingService = $charonGradingService;
         $this->userService = $userService;
         $this->resultRepository = $resultRepository;
         $this->testSuiteService = $testSuiteService;
+        $this->course = $course;
+        $this->studentsRepository = $studentsRepository;
     }
+    /* Function needed to get course id using git callback
+     * Takes ->
+     * 	- GitCallback $call
+     * Returns ->
+     *  - int $courseId
+     */
+    
+    public function getCourseIdFromGitCallBack(GitCallback $call)
+    {
+		$string = explode("/",$call->repo);
+		$string = $string[1];
+		$string = explode(".",$string);
+		$string = $string[0];
+		return $this->course->getCourseByName($string);
+	}
+	
+	/* Function needed to get list of students related to exact course
+	 * Takes ->
+	 *  - int $courseId
+	 * Returns ->
+	 *  - array $nameList
+	 */
+	 
+	public function getStudentsRelatedToCourse(int $courseId)
+    {
+		$nameList = null;
+		$studentList=json_decode(json_encode($this->studentsRepository->searchStudentsByCourseAndKeyword($courseId,"")),true);
+		for($i=0;$i<count($studentList);$i++)
+		{
+			$nameList[$i] = $studentList[$i]["username"];
+		}
+		return $nameList;
+	}
+	
+	/* Function needed to pass only users who belong to course
+	 * Returns only these array elements what exist in both arrays
+	 * Takes ->
+	 *  - Simple string array $usernames
+	 *  - Simple string array $filter
+	 * Returns ->
+	 *  - Simple string array $filtered
+	 * 
+	 */
+	public function usernamesFilter(array $usernames,array $filter)
+	{
+		sort($usernames);
+		sort($filter);
+		$filtered;
+		for($i=0;$i<count($usernames);$i++)
+		{
+			for($c=0;$c<count($filter);$c++)
+			{
+				if($usernames[$i]==$filter[$c])
+				{
+					$filtered[] = $usernames[$i];
+					break;
+				}
+			}
+		}
+		return $filtered;
+	}
+
+
 
     /**
      * Save a new submission from tester data.
@@ -66,7 +137,14 @@ class SaveTesterCallback
      */
     public function run(TesterCallbackRequest $request, GitCallback $gitCallback, array $usernames)
     {
+		//print_r('Usernames-> '.json_encode($usernames)."\n\n");
+        //print_r('Gitcallback-> '.json_encode($gitCallback)."\n\n");
+        $students = $this->getStudentsRelatedToCourse($this->getCourseIdFromGitCallBack($gitCallback));
+        //print_r('Related to course students-> '.json_encode($students)."\n\n");
+        $usernames = $this->usernamesFilter($usernames,$students);
+        //print_r('Passed users-> '.json_encode($usernames)."\n\n");
         $users = $this->getStudentsInvolved($usernames);
+        //print_r('Users-> '.json_encode($users)."\n\n");
 
         $submission = $this->createNewSubmission($request, $gitCallback, $users[0]->id);
 
