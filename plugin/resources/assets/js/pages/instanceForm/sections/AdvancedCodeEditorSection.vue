501c01c6c7109c96254ad12345402219f62d392e
Artur-Aleksander Pärnoja <arthurp200148@gmail.com>
2021-07-20 13:57:41 +0300

    #490 Add code editor to AdvancedCodeEditorSection.vue with hardcoded language.

--- plugin/resources/assets/js/pages/instanceForm/sections/AdvancedCodeEditorSection.vue
+++ plugin/resources/assets/js/pages/instanceForm/sections/AdvancedCodeEditorSection.vue
@@ -2,13 +2,46 @@
 
   <div class="fcontainer clearfix fitem">
 
-    <p>Advanced Code Editor</p>
+    <label>
+      <input id="setEditor" type="checkbox" name="editor_set" v-model="form.fields.editor_set" value="true">
+      Add code editor to this charon
+    </label>
+
+    <h2>Source Files</h2>
+
+    <v-btn class="ma-2 submitBtn" small tile outlined color="primary">
+      + Create Source File
+    </v-btn>
+
+    <p>Language: python</p>
+
+    <AceEditor
+        class="editor"
+        v-model="content"
+        @init="editorInit"
+        lang="python"
+        theme="crimson_editor"
+        width="100%"
+        height="500px"
+        :options="{
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        fontSize: 14,
+        highlightActiveLine: true,
+        enableSnippets: true,
+        showLineNumbers: true,
+        tabSize: 2,
+        showPrintMargin: false,
+        showGutter: true,
+        }"
+    />
 
   </div>
 
 </template>
 
 <script>
+import AceEditor from 'vuejs-ace-editor';
 
 export default {
 
@@ -16,6 +49,35 @@ export default {
 
   props: {
     form: {required: true}
+  },
+
+  components: {
+    AceEditor
+  },
+
+  data() {
+    return {
+      content: '',
+    }
+  },
+
+  methods: {
+    editorInit: function () {
+      require('brace/ext/language_tools')//language extension prerequsite...
+      require('brace/mode/html')
+      require('brace/mode/python')//language
+      require('brace/mode/javascript')
+      require('brace/mode/java')
+      require('brace/mode/prolog')
+      require('brace/mode/csharp')
+      require('brace/mode/less')
+      require('brace/theme/crimson_editor')
+      require('brace/snippets/python')//snippet
+      require('brace/snippets/javascript')
+      require('brace/snippets/java')
+      require('brace/snippets/prolog')
+      require('brace/snippets/csharp')
+    }
   },
 
 }
