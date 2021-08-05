115e18279d7c8e011e2743fedc690aa02a25797c
Nikita Birjukovs <nibirj@ttu.ee>
2021-08-05 15:29:53 +0300

    #524 copying to clipboard is done

--- plugin/resources/assets/js/pages/assignment/components/CodeEditor.vue
+++ plugin/resources/assets/js/pages/assignment/components/CodeEditor.vue
@@ -24,7 +24,9 @@
         showGutter: true,
         }"
     />
-
+    <v-btn class="ma-2 submitBtn" small tile outlined color="primary" @click="copyToClipBoard">
+      Copy
+    </v-btn>
   </div>
 </template>
 
@@ -58,7 +60,45 @@ export default {
   },
 
   methods: {
+    copyToClipBoard() {
+      const id = "mycustom-clipboard-textarea-hidden-id";
+      let existsTextarea = document.getElementById(id);
 
+      if(!existsTextarea){
+        const textarea = document.createElement("textarea");
+        textarea.id = id;
+        // Place in top-left corner of screen regardless of scroll position.
+        textarea.style.position = 'fixed';
+        textarea.style.top = 0;
+        textarea.style.left = 0;
+        // Ensure it has a small width and height. Setting to 1px / 1em
+        // doesn't work as this gives a negative w/h on some browsers.
+        textarea.style.width = '1px';
+        textarea.style.height = '1px';
+        // We don't need padding, reducing the size if it does flash render.
+        textarea.style.padding = 0;
+        // Clean up any borders.
+        textarea.style.border = 'none';
+        textarea.style.outline = 'none';
+        textarea.style.boxShadow = 'none';
+        // Avoid flash of white box if rendered for any reason.
+        textarea.style.background = 'transparent';
+        document.querySelector("body").appendChild(textarea);
+        existsTextarea = document.getElementById(id);
+      }
+      existsTextarea.value = this.content;
+      existsTextarea.select();
+      try {
+        const status = document.execCommand('copy');
+        if(!status){
+          VueEvent.$emit('show-notification', 'Cannot copy text.\n', 'danger');
+        }else{
+          VueEvent.$emit('show-notification', 'The text is now on the clipboard.\n');
+        }
+      } catch (err) {
+        VueEvent.$emit('show-notification', 'Unable to copy.\n' + err, 'danger');
+      }
+    },
     /**
      * Ace-code editor now supports only html, python, javascript, java, prolog and C#,
      * but more languages in these method like these: require('brace/mode/language'), where
