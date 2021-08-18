a0ca763370335809e612f1999c28c2398dfa4811
ilja89 <52709138+ilja89@users.noreply.github.com>
2021-08-18 20:43:53 +0300

    #573 Finished

--- plugin/resources/assets/js/pages/assignment/components/StudentRegistrations.vue
+++ plugin/resources/assets/js/pages/assignment/components/StudentRegistrations.vue
@@ -43,7 +43,7 @@
                 <v-btn icon @click="deleteItem(item)">
                   <img alt="eye" height="24px" src="pix/bin.png" width="24px">
                 </v-btn>
-                <registration-queue-sheet :labData="item"/>
+                <registration-queue-sheet v-if="showQueueButton(item)" :labData="item"/>
               </template>
             </v-data-table>
           </v-flex>
@@ -89,7 +89,21 @@ export default {
   },
 
   methods: {
-    deleteItem(item) {
+			showQueueButton({lab_end,progress}) {
+				if (progress !== "Waiting") {
+					return false;
+				}
+				const dateNow = new Date();
+				let dateEnd = lab_end.split(" ");
+				dateEnd = dateEnd[0].split("-").concat(dateEnd[1].split("-"));
+				dateEnd = new Date(dateEnd[0], dateEnd[1] - 1, dateEnd[2], dateEnd[3].split(":")[0], dateEnd[3].split(":")[1]);
+				if (dateNow.getTime() > dateEnd.getTime()) {
+					return false;
+				}
+				return true;
+			},
+
+			deleteItem(item) {
       if (this.dateValidation(item)) {
         if (confirm(this.translate("registrationDeletionConfirmationText"))) {
           this.deleteReg(item);
