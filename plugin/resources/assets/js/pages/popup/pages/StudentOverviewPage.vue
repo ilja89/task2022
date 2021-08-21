0539b2bb11f3d93e130558954f3672c89873a5f8
ilja89 <52709138+ilja89@users.noreply.github.com>
2021-08-21 14:15:31 +0300

    Done

--- plugin/resources/assets/js/pages/popup/pages/StudentOverviewPage.vue
+++ plugin/resources/assets/js/pages/popup/pages/StudentOverviewPage.vue
@@ -1,106 +1,109 @@
 <template>
-    <div class="student-overview-container">
-        <page-title :student="student"></page-title>
+	<div class="student-overview-container">
+		<page-title :student="student"></page-title>
 
-        <popup-section title="Grades report"
-                       subtitle="Grading report for the current student.">
+		<popup-section title="Grades report"
+									 subtitle="Grading report for the current student.">
 
-            <v-card class="mx-auto" outlined light raised>
-                <v-container class="spacing-playground pa-3" fluid>
-                    <div class="student-overview-card" v-html="table"></div>
-                </v-container>
-            </v-card>
+			<v-card class="mx-auto" outlined light raised >
+				<v-container class="spacing-playground pa-3" fluid>
+					<div class="student-overview-card" v-html="table"></div>
+				</v-container>
+			</v-card>
 
-        </popup-section>
-    </div>
+		</popup-section>
+	</div>
 </template>
 
 <script>
-    import {PageTitle} from '../partials'
-    import {mapState, mapGetters, mapActions} from 'vuex'
-    import {User} from '../../../api'
-    import {PopupSection} from '../layouts'
+import {PageTitle} from '../partials'
+import {mapState, mapGetters, mapActions} from 'vuex'
+import {User} from '../../../api'
+import {PopupSection} from '../layouts'
 
-    export default {
+export default {
 
-        components: {PopupSection, PageTitle},
+	components: {PopupSection, PageTitle},
 
-        data() {
-            return {
-                table: '',
-            }
-        },
+	data() {
+		return {
+			table: '',
+			CssOverflowWrapStyle:`<style type="text/css">.gradeitemheader {overflow-wrap:anywhere !important;word-break:break-all !important;}
+th[class="level2 leveleven oddd2 b1b b1t column-itemname"]{overflow-wrap:anywhere !important;word-break:break-all !important;}
+th[class="level2 leveleven evend2 b1b b1t column-itemname"]{overflow-wrap:anywhere !important;word-break:break-all !important;}</style>`,
+		}
+	},
 
-        computed: {
-            ...mapState([
-                'student',
-            ]),
+	computed: {
+		...mapState([
+			'student',
+		]),
 
-            ...mapGetters([
-                'courseId',
-            ]),
+		...mapGetters([
+			'courseId',
+		]),
 
-            routeStudentId() {
-                return this.$route.params.student_id
-            },
-        },
+		routeStudentId() {
+			return this.$route.params.student_id
+		},
+	},
 
-        watch: {
-            $route() {
-                if (typeof this.routeStudentId !== 'undefined' && this.$route.name === 'student-overview') {
-                    this.getStudent()
-                    this.getStudentOverviewTable()
-                }
-            },
-        },
+	watch: {
+		$route() {
+			if (typeof this.routeStudentId !== 'undefined' && this.$route.name === 'student-overview') {
+				this.getStudent()
+				this.getStudentOverviewTable()
+			}
+		},
+	},
 
-        methods: {
-            ...mapActions([
-                'fetchStudent',
-            ]),
+	methods: {
+		...mapActions([
+			'fetchStudent',
+		]),
 
-            getStudentOverviewTable() {
-                User.getReportTable(this.courseId, this.routeStudentId, (table) => {
-                    this.table = table
-                })
-            },
+		getStudentOverviewTable() {
+			User.getReportTable(this.courseId, this.routeStudentId, (table) => {
+				this.table = this.CssOverflowWrapStyle + table;
+			})
+		},
 
-            getStudent() {
-                this.fetchStudent({courseId: this.courseId, studentId: this.routeStudentId})
-            },
-        },
+		getStudent() {
+			this.fetchStudent({courseId: this.courseId, studentId: this.routeStudentId})
+		},
+	},
 
-        created() {
-            this.getStudent()
-            this.getStudentOverviewTable()
-        },
-    }
+	created() {
+		this.getStudent()
+		this.getStudentOverviewTable()
+	},
+}
 </script>
 
 <style lang="scss">
 
-    .student-overview-card {
-        padding: 25px;
-    }
+.student-overview-card {
+	padding: 25px;
+}
 
-    .b1l {
-        padding: 0;
-        width: 24px;
-        min-width: 24px;
-    }
+.b1l {
+	padding: 0;
+	width: 24px;
+	min-width: 24px;
+}
 
-    .student-overview-card {
-        th {
-            padding: 0.75em;
+.student-overview-card {
+	th {
+		padding: 0.75em;
 
-            img {
-                margin-right: 15px;
-            }
-        }
+		img {
+			margin-right: 15px;
+		}
+	}
 
-        td {
-            padding: 0.75em;
-        }
-    }
+	td {
+		padding: 0.75em;
+	}
+}
 
 </style>
