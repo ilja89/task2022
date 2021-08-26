7b8af01262692e4c13476a4f2207511538f3b752
ilja89 <52709138+ilja89@users.noreply.github.com>
2021-08-26 17:29:49 +0300

    #517 fixing condition check:Lab must be actual and registration must have 'Waiting' status

--- plugin/app/Repositories/LabRepository.php
+++ plugin/app/Repositories/LabRepository.php
@@ -504,7 +504,10 @@ class LabRepository
         return \DB::table('charon_defenders')
             ->join("charon", "charon.id", "charon_defenders.charon_id")
             ->join("charon_defense_lab","charon_defense_lab.id","charon_defenders.defense_lab_id")
+            ->join("charon_lab","charon_lab.id","charon_defense_lab.lab_id")
             ->where("charon_defense_lab.lab_id", $labId)
+            ->where("charon_lab.end",">=",Carbon::now())
+            ->where("charon_defenders.progress","Waiting")
             ->select("charon.name as charon_name", "charon.defense_duration as charon_length", "charon_defenders.student_id")
             ->orderBy("charon_defenders.id", "asc")
             ->get();
