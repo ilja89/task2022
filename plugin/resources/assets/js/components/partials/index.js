698efb4f35f6db2b78db35bde4557d8e1126d164
divahe <divahe@ttu.ee>
2021-09-09 13:07:58 +0300

    #607 noCommentsComponent removed, tabs fixed

--- plugin/resources/assets/js/components/partials/index.js
+++ plugin/resources/assets/js/components/partials/index.js
@@ -4,9 +4,7 @@ import Datepicker from './Datepicker.vue'
 import FilesComponent from './FilesComponent.vue'
 import FilesComponentWithoutTree from './FilesComponentWithoutTree.vue'
 import CommentComponent from "./CommentComponent";
-import NoCommentsComponent from "./NoCommentsComponent";
 
 export {
-    CharonTab, CharonTabs, Datepicker, FilesComponent, FilesComponentWithoutTree, CommentComponent,
-    NoCommentsComponent
+    CharonTab, CharonTabs, Datepicker, FilesComponent, FilesComponentWithoutTree, CommentComponent
 }
