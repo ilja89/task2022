7e403109be156db10a89c51658b80238b43be1a2
divahe <divahe@ttu.ee>
2021-10-04 20:01:38 +0300

    Refresh error fixed part II

--- plugin/resources/assets/js/components/partials/ReviewCommentComponent.vue
+++ plugin/resources/assets/js/components/partials/ReviewCommentComponent.vue
@@ -2,7 +2,7 @@
     <v-card>
         <v-container>
             <review-comments
-                v-for="file in files" key="file.id"
+                v-for="file in files" :key="file.id"
                 v-if="file.review_comments.length > 0"
                 :file="file" :view="view"
             >
