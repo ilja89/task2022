f7732939846f72aabc4a8b2e866321f936754658
Jüri Helekivi <Juri.Helekivi@gmail.com>
2021-10-28 12:47:18 +0300

    #685 Continue with dev-setup if git fails

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,5 +1,5 @@
-git config core.filemode false
-git checkout -- .htaccess
+git config core.filemode false || :
+git checkout -- .htaccess || :
 cp composer.phar /opt/bitnami/php/bin
 composer install
 npm ci
