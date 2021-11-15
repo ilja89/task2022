f51090f8617651113d8445c238eca4c49e9bd807
David Avedis Injarabian <david.avedis.inj@gmail.com>
2021-11-15 13:12:45 +0200

    #680 Removed old/unused files that were not used. Charon dashboard was implemented into the Popup, therefore these files are not unnecessary.

--- plugin/resources/views/charon_dashboard/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<script src="/mod/charon/plugin/public/js/highlight.js"></script>
-<script>
-    window.hljs = hljs;
-</script>
-<script src="/mod/charon/plugin/public/js/charonDashboard.js"></script>
-
-</body>
-</html>
