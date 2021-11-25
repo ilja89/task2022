7d8688f5962bd93b34979583560179214b9ad4f2
divahe <divahe@ttu.ee>
2021-11-25 16:32:49 +0200

    #706 css adjusted for compact submission info

--- plugin/resources/assets/js/pages/popup/partials/Submission.vue
+++ plugin/resources/assets/js/pages/popup/partials/Submission.vue
@@ -10,15 +10,17 @@
                  overlap
                  left
                  offset-x="-1"
-        ><div class="submission-str">{{ submissionString }}</div></v-badge>
+        >
 
         <div class="submission-timestamps">
+            <span class="submission-str">{{ submissionString }}</span>
+            <wbr>
             <span class='timestamp-info'>Git: </span>{{ this.submission.git_timestamp }}
             <wbr>
             <span class="timestamp-separator"> | </span>
             <wbr>
             <span class='timestamp-info'>Moodle: </span>{{ this.submission.created_at }}
-        </div>
+        </div></v-badge>
 
         <span class="confirmed-check">
             <div v-if="submission.confirmed === 1" class="confirmed-check-check"></div>
@@ -52,3 +54,24 @@
         },
     }
 </script>
+
+<style scoped>
+
+    .submission-timestamps {
+        line-height: 1.5;
+    }
+
+    .submission-str {
+        padding-right: 0.5em;
+    }
+
+    .submission {
+        padding-top: 1em!important;
+        padding-bottom: 1em!important;
+        padding-right: 1em!important;
+        margin-top: 1em;
+        margin-bottom: 1em;
+        flex-direction: unset;
+    }
+
+</style>
