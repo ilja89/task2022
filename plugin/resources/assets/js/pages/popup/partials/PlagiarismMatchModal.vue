4558fb64caf69d2338c8ed57fe14d873ebff64b2
jjaaks <jjaaks@ttu.ee>
2022-02-24 13:35:45 +0200

    #787 Added better modal

--- plugin/resources/assets/js/pages/popup/partials/PlagiarismMatchModal.vue
+++ plugin/resources/assets/js/pages/popup/partials/PlagiarismMatchModal.vue
@@ -14,7 +14,7 @@
 
     <v-card style="background-color: white; overflow-y: auto;">
       <v-toolbar dark>
-        <span class="headline">Modal information here</span>
+        <span class="headline">({{this.match.percentage}}%) - ({{this.match.other_percentage}}%)</span>
 
         <v-spacer></v-spacer>
 
@@ -24,8 +24,16 @@
       </v-toolbar>
 
       <v-card-text class="pt-4">
-        <div>
-          {{ this.match }}
+        <a v-bind:href="this.match.moss_url" target="_blank">Moss match url</a><br><br>
+        <h1 v-if="toggleShowSimilarities">Showing full files</h1>
+        <h1 v-else>Showing only similar sections</h1>
+        <br>
+        <toggle-button @buttonClicked="showSimilarities($event)"></toggle-button>
+        <div v-if="!toggleShowSimilarities">
+          <match-files-component :match="this.match" :tester-type="testerType"></match-files-component>
+        </div>
+        <div v-if="toggleShowSimilarities">
+          <match-similarities-component :similarities="this.match.similarities" :tester-type="testerType" :uniid="this.match.uniid" :other_uniid="this.match.other_uniid"></match-similarities-component>
         </div>
       </v-card-text>
     </v-card>
@@ -34,6 +42,9 @@
 
 <script>
 import {Translate} from '../../../mixins'
+import {ToggleButton} from "../../../components/partials";
+import MatchFilesComponent from "../../../components/partials/MatchFilesComponent";
+import MatchSimilaritiesComponent from "../../../components/partials/MatchSimilaritiesComponent";
 
 export default {
   name: "plagiarism-match-modal",
@@ -41,6 +52,9 @@ export default {
   mixins: [Translate],
 
   components: {
+    MatchSimilaritiesComponent,
+    MatchFilesComponent,
+    ToggleButton
   },
 
   props: {
@@ -50,10 +64,51 @@ export default {
   data() {
     return {
       isActive: false,
+      toggleShowSimilarities: false,
+      testerType: "python"
     }
   },
 
   computed: {
+    activeFile() {
+      let match = this.match;
+
+      return {
+        contents: match.code.trim().replace(/</g, '&lt;').replace(/>/g, '&gt;'),
+        numbers: match.code.trim().split(/\r\n|\r|\n/).length,
+      }
+    },
+    activeOtherFile() {
+      let match = this.match
+
+      return {
+        contents: match.other_code.trim().replace(/</g, '&lt;').replace(/>/g, '&gt;'),
+        numbers: match.other_code.trim().split(/\r\n|\r|\n/).length,
+      }
+    },
+    activeSimilarity() {
+      let similarity = this.match.similarities.find(similarity => {
+        return similarity.id === this.activeSimilarityId;
+      });
+      let lines_count = similarity.lines.split('-');
+      let other_lines_count = similarity.other_lines.split('-');
+      let lines = [];
+      let other_lines = [];
+      for (let i = parseInt(lines_count[0] - 1); i <= parseInt(lines_count[1]); i++) {
+        lines.push(i)
+      }
+      for (let i = parseInt(other_lines_count[0] - 1); i <= parseInt(other_lines_count[1]); i++) {
+        other_lines.push(i)
+      }
+
+      return {
+        id: similarity.id,
+        code_block: similarity.code_block,
+        other_code_block: similarity.other_code_block,
+        lines: lines,
+        other_lines: other_lines
+      }
+    },
   },
 
   mounted() {
@@ -62,6 +117,9 @@ export default {
   methods: {
     onClickMatchInformation() {
       this.isActive = true;
+    },
+    showSimilarities(bool) {
+      this.toggleShowSimilarities = bool;
     },
   },
 }
