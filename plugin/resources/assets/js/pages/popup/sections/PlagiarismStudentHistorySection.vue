7aad145372c08a30f28a2084974c8dd19a8296ef
David Avedis Injarabian <david.avedis.inj@gmail.com>
2022-04-16 18:05:43 +0300

    #806 Updated table. Added new statistics table. Added graph to display amounts of different statuses per charon.

--- plugin/resources/assets/js/pages/popup/sections/PlagiarismStudentHistorySection.vue
+++ plugin/resources/assets/js/pages/popup/sections/PlagiarismStudentHistorySection.vue
@@ -11,10 +11,10 @@
             </v-btn>
         </template>
 
-        <div>
+        <v-card v-if="matchesFetched">
             <v-card-title>
                 <v-text-field
-                    v-model="search"
+                    v-model="searchAllMatches"
                     append-icon="mdi-magnify"
                     label="Search"
                     style="width:60%;float: left;padding-right: 10px"
@@ -29,20 +29,17 @@
                     style="width:10%;padding-left: 10px"
                 ></v-select>
             </v-card-title>
-        </div>
-
-        <div>
             <v-data-table
                 class="center-table"
-                :headers="headers"
+                :headers="headersAllMatches"
                 :items="matches"
-                :search="search"
+                :search="searchAllMatches"
                 :footer-props="{
                     'items-per-page-options': [10, 25, 50, -1]
                 }"
                 :items-per-page="25"
-                :sort-by.sync="sortBy"
-                :sort-desc.sync="sortDesc"
+                :sort-by.sync="sortForAllMatches"
+                :sort-desc.sync="sortDescForAllMatches"
             >
                 <template v-slot:item.status="{ item }">
                     <v-chip
@@ -69,7 +66,26 @@
                     </v-row>
                 </template>
             </v-data-table>
-        </div>
+        </v-card>
+
+        <v-card v-if="matchesFetched" class="mt-16">
+            <v-data-table
+                class="center-table"
+                :headers="headersStatistics"
+                :items="statistics"
+                :footer-props="{
+                    'items-per-page-options': [10, 25, 50, -1]
+                }"
+                :items-per-page="10"
+                :sort-by.sync="sortForStatistics"
+                :sort-desc.sync="sortDescForStatistics"
+            ></v-data-table>
+        </v-card>
+
+        <v-card class="mt-16">
+            <apexcharts type="bar" height="500" :options="chartOptions" :series="chartSeries"
+                        v-show="matchesFetched"></apexcharts>
+        </v-card>
 
     </popup-section>
 </template>
@@ -79,14 +95,15 @@ import PlagiarismMatchModal from "../partials/PlagiarismMatchModal";
 import {PopupSection} from '../layouts';
 import {Plagiarism} from "../../../api";
 import {mapGetters} from "vuex";
+import VueApexCharts from 'vue-apexcharts';
 
 export default {
-    components: {PopupSection, PlagiarismMatchModal},
+    components: {PopupSection, PlagiarismMatchModal, 'apexcharts': VueApexCharts},
     props: ['student'],
 
     data() {
         return {
-            search: '',
+            searchAllMatches: '',
             status: '',
             select: {status: 'All', abbr: ''},
             selectItems: [
@@ -96,21 +113,25 @@ export default {
                 {status: 'Plagiarism', abbr: 'plagiarism'},
             ],
             matches: [],
-            sortBy: 'created_timestamp',
-            sortDesc: false
+            statistics: [],
+            sortForAllMatches: 'created_timestamp',
+            sortForStatistics: 'max_lines_matched',
+            sortDescForAllMatches: false,
+            sortDescForStatistics: true,
+            matchesFetched: false
         }
     },
 
     computed: {
-        headers() {
+        headersAllMatches() {
             return [
                 {text: 'Created at', align: 'start', value: 'created_timestamp'},
-                {text: 'Charon', value: 'assignment_name'},
-                {text: 'Lines matched', value: 'lines_matched'},
-                {text: 'Uni-ID', value: 'uniid'},
-                {text: 'Percentage', value: 'percentage'},
-                {text: 'Other Uni-ID', value: 'other_uniid'},
-                {text: 'Other Percentage', value: 'other_percentage'},
+                {text: 'Charon', align: 'center', value: 'assignment_name'},
+                {text: 'Lines matched', align: 'center', value: 'lines_matched'},
+                {text: 'Uni-ID', align: 'center', value: 'uniid'},
+                {text: 'Percentage', align: 'center', value: 'percentage'},
+                {text: 'Other Uni-ID', align: 'center', value: 'other_uniid'},
+                {text: 'Other Percentage', align: 'center', value: 'other_percentage'},
                 {
                     text: 'Status', value: 'status', filter: value => {
                         if (!this.status) return true
@@ -122,8 +143,81 @@ export default {
             ]
         },
 
+        headersStatistics() {
+            return [
+                {text: 'Charon', align: 'start', value: 'assignment_name'},
+                {text: 'Max lines matched', align: 'center', value: 'max_lines_matched'},
+                {text: 'Max percentage', align: 'center', value: 'max_percentage'},
+                {text: 'Max other percentage', align: 'center', value: 'max_other_percentage'},
+                {text: 'New amount', align: 'center', value: 'new_amount'},
+                {text: 'Acceptable amount', align: 'center', value: 'acceptable_amount'},
+                {text: 'Plagiarism amount', align: 'center', value: 'plagiarism_amount'}
+            ]
+        },
+
         showStudentHistoryText() {
             return 'Fetch student matches'
+        },
+
+        chartOptions() {
+            return {
+                chart: {
+                    type: 'bar',
+                    height: 500,
+                    stacked: true,
+                    toolbar: {
+                        show: true
+                    },
+                    zoom: {
+                        enabled: true
+                    }
+                },
+                plotOptions: {
+                    bar: {
+                        horizontal: false,
+                        borderRadius: 10
+                    },
+                },
+                title: {
+                    text: "Student's plagiarism progress for each tested Charon",
+                    align: 'left'
+                },
+                xaxis: {
+                    type: 'category',
+                    categories: this.statistics.map(charon_statistics => {
+                        return charon_statistics.assignment_name
+                    }),
+                },
+                legend: {
+                    position: 'right',
+                    offsetY: 40
+                },
+                fill: {
+                    opacity: 1
+                },
+                colors: ['#8e8e8e', '#56a576', '#f44336']
+            }
+        },
+
+        chartSeries() {
+            return [
+                {
+                    name: 'new',
+                    data: this.statistics.map(charon_statistics => {
+                        return charon_statistics.new_amount
+                    })
+                }, {
+                    name: 'acceptable',
+                    data: this.statistics.map(charon_statistics => {
+                        return charon_statistics.acceptable_amount
+                    })
+                }, {
+                    name: 'plagiarism',
+                    data: this.statistics.map(charon_statistics => {
+                        return charon_statistics.plagiarism_amount
+                    })
+                }
+            ]
         },
 
         ...mapGetters([
@@ -141,46 +235,91 @@ export default {
         fetchStudentMatches() {
             Plagiarism.fetchStudentMatches(this.courseId, this.student.username, (response) => {
                 this.matches = response
-                this.formatMatches(response)
+
+                if (this.matches.length) {
+                    this.prepareData(response)
+                    this.matchesFetched = true
+                }
             })
         },
 
-        formatMatches(matchesFromResponse) {
-            return matchesFromResponse.map(match => {
+        swapStudents(match) {
+            [
+                match.code,
+                match.commit_hash,
+                match.gitlab_commit_at,
+                match.percentage,
+                match.submission,
+                match.uniid,
+                match.other_code,
+                match.other_commit_hash,
+                match.other_gitlab_commit_at,
+                match.other_percentage,
+                match.other_submission,
+                match.other_uniid
+            ] =
+                [
+                    match.other_code,
+                    match.other_commit_hash,
+                    match.other_gitlab_commit_at,
+                    match.other_percentage,
+                    match.other_submission,
+                    match.other_uniid,
+                    match.code,
+                    match.commit_hash,
+                    match.gitlab_commit_at,
+                    match.percentage,
+                    match.submission,
+                    match.uniid,
+                ]
+        },
+
+        updateStatistics(matchesStatistics, match) {
+            let charon_name = match.assignment_name
+            if (charon_name in matchesStatistics) {
+                if (match.lines_matched > matchesStatistics[charon_name]["max_lines_matched"]) {
+                    matchesStatistics[charon_name]["max_lines_matched"] = match.lines_matched
+                }
+                if (match.percentage > matchesStatistics[charon_name]["max_percentage"]) {
+                    matchesStatistics[charon_name]["max_percentage"] = match.percentage
+                }
+                if (match.other_percentage > matchesStatistics[charon_name]["other_percentage"]) {
+                    matchesStatistics[charon_name]["max_other_percentage"] = match.other_percentage
+                }
+                if (match.status === "new") {
+                    matchesStatistics[charon_name]["new_amount"] = matchesStatistics[charon_name]["new_amount"] + 1
+                }
+                if (match.status === "acceptable") {
+                    matchesStatistics[charon_name]["acceptable_amount"] = matchesStatistics[charon_name]["acceptable_amount"] + 1
+                }
+                if (match.status === "plagiarism") {
+                    matchesStatistics[charon_name]["plagiarism_amount"] = matchesStatistics[charon_name]["plagiarism_amount"] + 1
+                }
+            } else {
+                matchesStatistics[charon_name] = {
+                    "assignment_name": match.assignment_name,
+                    "max_lines_matched": match.lines_matched,
+                    "max_percentage": match.percentage,
+                    "max_other_percentage": match.other_percentage,
+                    "new_amount": (match.status === "new") ? 1 : 0,
+                    "acceptable_amount": (match.status === "acceptable") ? 1 : 0,
+                    "plagiarism_amount": (match.status === "plagiarism") ? 1 : 0
+                }
+            }
+        },
+
+        prepareData(matchesFromResponse) {
+            let matchesStatistics = {}
+            matchesFromResponse.map(match => {
                 match.created_timestamp = match.created_timestamp.split('.')[0]
 
                 if (match.other_uniid === this.student.username.split('@')[0]) {
-                    [
-                        match.code,
-                        match.commit_hash,
-                        match.gitlab_commit_at,
-                        match.percentage,
-                        match.submission,
-                        match.uniid,
-                        match.other_code,
-                        match.other_commit_hash,
-                        match.other_gitlab_commit_at,
-                        match.other_percentage,
-                        match.other_submission,
-                        match.other_uniid
-                    ] =
-                        [
-                            match.other_code,
-                            match.other_commit_hash,
-                            match.other_gitlab_commit_at,
-                            match.other_percentage,
-                            match.other_submission,
-                            match.other_uniid,
-                            match.code,
-                            match.commit_hash,
-                            match.gitlab_commit_at,
-                            match.percentage,
-                            match.submission,
-                            match.uniid,
-                        ]
+                    this.swapStudents(match)
                 }
+                this.updateStatistics(matchesStatistics, match)
                 return match
             })
+            this.statistics = Object.values(matchesStatistics)
         },
 
         getColor(status) {
