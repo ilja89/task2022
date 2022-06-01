6b75256054bf32af0e8d28cd4eafafb51296ba9d
vjkolo <vjkolo@ttu.ee>
2022-06-02 02:52:13 +0300

    #861 Fixed code template

--- plugin/resources/views/assignment/index.blade.php
+++ plugin/resources/views/assignment/index.blade.php
@@ -6,9 +6,10 @@
 
 <div class="rows assignment-rows" id="app">
     <div class="columns assignment-columns">
-        <div class="column is-two-thirds assignment-content content" v-highlightjs v-pre>
+        <div class="column is-two-thirds assignment-content content" v-highlightjs>
 
-            {!! rewritePluginTextUrls($charon->description, 'description', $course_module_id) !!}
+
+            <div v-pre> {!! rewritePluginTextUrls($charon->description, 'description', $course_module_id) !!} </div>
 
             <div class="edit-container" v-if="!(allow_submission > 0)">
                 <code-templates
